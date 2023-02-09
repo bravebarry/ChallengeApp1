cdd356b10fe0fa2047754b58e21ee6e06e100bf3
bravebarry <wojciechowskimarcink@gmail.com>
2023-02-09 15:25:52 +0100

    Dodaj pliki projektów.

---
+++ ChallengeApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ChallengeApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj imię:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Podaj płeć (Kobieta/Mężczyzna):");
+            string gender = Console.ReadLine();
+            Console.WriteLine("Podaj wiek:");
+            int age = int.Parse(Console.ReadLine());
+
+            if (gender == "Kobieta" && age < 30)
+            {
+                Console.WriteLine("Kobieta poniżej 30 lat");
+            }
+            else if (name == "Ewa" && age == 33)
+            {
+                Console.WriteLine("Ewa, lat 33");
+            }
+            else if (gender == "Mężczyzna" && age < 18)
+            {
+                Console.WriteLine("Niepełnoletni Mężczyzna");
+            }
+        }
+    }
+}
